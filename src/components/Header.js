c7df32fef3d8dc4e82f14d54e0cf2222e14d7714
emandalapu11 <145249603+emandalapu11@users.noreply.github.com>
2023-11-09 01:37:40 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+    <div className='container1'> 
+        
+        <nav>
+  
+    <div class="sticky top-0  max-w-screen-xl flex items-center justify-between ">
+    <a href="https://metaculars.com/" class="flex items-center">
+
+    <svg width="30" height="30" viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M87.0499 78.7875C86.0615 79.4024 84.7823 78.6922 84.7815 
+77.5282L84.7649 50.9653C84.7649 50.5107 84.3293 50.1697 83.8625
+ 50.1697C83.3646 50.1697 82.9912 50.5107 82.9912 50.9653L83.0092 
+ 79.7177C83.0098 80.7023 82.5018 81.6174 81.6658 82.1376C79.7695 
+ 83.3175 77.3148 81.9549 77.3133 79.7214L77.2967 55.6819C77.2967
+  55.2557 76.8921 54.8863 76.4254 54.8863C75.9275 54.8863 75.5541 
+  55.2557 75.5541 55.6819L75.5739 84.3179C75.5746 85.3187 75.0582 
+  86.2489 74.2085 86.7776C72.2812 87.9769 69.7863 86.5921 69.7846 
+  84.3222L69.7662 60.4837C69.7662 60.0575 69.3929 59.6881 68.8949 
+  59.6881C68.3971 59.6881 68.0236 60.0575 68.0236 60.4837L68.0456 
+  89.0193C68.0464 90.0094 67.5356 90.9296 66.695 91.4527C64.7886 
+  92.6389 62.3208 91.2694 62.3188 89.024L62.298 65.2003C62.298 64.7457 
+  61.9246 64.4047 61.4267 64.4047C60.9648 64.4047 60.5639 64.7386 
+  60.5555 65.1861C60.5554 65.1955 60.5554 65.1944 60.5554 65.2038C60.5557 
+  65.4936 60.5714 83.5217 60.5804 93.8195C60.581 94.5075 60.0071 95.0792 
+  59.4345 94.6975L33.9785 78.7875C33.5239 78.4465 33.1829 77.9919 33.1829 
+  77.3101V71.6159V45.6899C33.1829 45.008 33.6375 44.4398 34.0921
+   44.2125L59.6618 28.3025C60.3437 27.8479 61.0256 27.8479 61.7074 
+   28.3025L87.1635 44.2125C87.6181 44.5534 87.959 45.008 87.959 
+   45.6899V71.6159V77.3101C87.959 77.9919 87.5044 78.5602 87.0499
+    78.7875ZM15 51.3841V67.0822V85.2651V87.9926C15 88.6744 15.4546 
+    89.2426 15.9092 89.4699L57.7391 115.614C58.9434 116.366 60.4577 
+    117.596 61.5938 116.744L105.233 89.4699C105.801 89.0153 106.142 
+    88.5608 106.142 87.9926V85.2651V67.0822V51.3841V37.7349V35.0074C106.142 
+    34.3256 105.687 33.7573 105.233 33.5301L63.4028 7.38631C62.1986 6.63367 
+    60.6843 5.40368 59.5482 6.25567L15.9092 33.5301C15.341 33.9846 15 34.4392 
+    15 35.0074V37.7349V51.3841Z" fill="#FBFBFB"/>
+</svg>
+<span class="self-center ml-3  text-white font-bold whitespace-nowrap light:text-white ">
+        Metaculars
+ </span></a>
+      
+
+  <div class="flex md:order-2">
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+       focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 
+       text-center mr-2 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Request a Demo</button>
+      <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center
+       p-1 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100
+        focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700
+         dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" 
+        viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+             stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+    <ul class="flex flex-col font-medium p-6 md:p-0 mt-4  md:flex-row md:space-x-6 md:mt-0 
+    md:border-0 md:bg-blue dark:bg-gray-800 md:dark:bg-gray-900 ">
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 
+        text-white bg-white-700 rounded md:bg-transparent md:text-white-700 
+        md:p-0 md:dark:text-white-500" aria-current="page">About</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 
+        text-white rounded hover:bg-gray-100 md:hover:bg-transparent
+         md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 
+         dark:text-white dark:hover:bg-gray-700 dark:hover:text-white 
+         md:dark:hover:bg-transparent dark:border-gray-700">Features</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 
+        text-white rounded hover:bg-gray-100 md:hover:bg-transparent
+         md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 
+         dark:text-white dark:hover:bg-gray-700 dark:hover:text-white 
+         md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+      </li> 
+   </ul>
+   <li id="li-right">
+   <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 
+   md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 
+   dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent 
+   dark:border-gray-700">Login</a>
+   </li>
+  </div>
+  </div>
+</nav>
+</div>
+      
+  )
+}
+
+export default Header;
